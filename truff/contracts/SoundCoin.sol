12a0a8bf302b62dda09b8695c92e8b5ebd70bc23
Anmol Singh <anmolshl2@gmail.com>
2018-06-19 16:13:58 +0200

    Updated contract

--- truff/contracts/SoundCoin.sol
+++ truff/contracts/SoundCoin.sol
@@ -1,5 +1,3 @@
-pragma solidity ^0.4.4;
-
 contract Token {
 
     /// @return total amount of tokens
@@ -42,8 +40,8 @@ contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
-            balances[_to] = SafeMath.add(balances[_to], _value);
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
             emit Transfer(msg.sender, _to, _value);
             return true;
         } else { return false; }
@@ -51,9 +49,9 @@ contract StandardToken is Token {
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] = SafeMath.add(balances[_to], _value);
-            balances[_from] = SafeMath.sub(balances[_from], _value);
-            allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
             emit Transfer(_from, _to, _value);
             return true;
         } else { return false; }
@@ -93,7 +91,7 @@ contract SoundCoin is StandardToken {
         totalSupply = 1000000000000000000000;
         name = "SoundCoin";
         decimals = 18;
-        symbol = "SDC";
+        symbol = "SNC";
         unitsOneEthCanBuy = 10;
         fundsWallet = msg.sender;
     }
@@ -103,8 +101,8 @@ contract SoundCoin is StandardToken {
         uint256 amount = msg.value * unitsOneEthCanBuy;
         require(balances[fundsWallet] >= amount);
 
-        balances[fundsWallet] = SafeMath.sub(balances[fundsWallet], amount);
-        balances[msg.sender] = SafeMath.add(balances[msg.sender], amount);
+        balances[fundsWallet] = balances[fundsWallet] - amount;
+        balances[msg.sender] = balances[msg.sender] + amount;
 
         emit Transfer(fundsWallet, msg.sender, amount);
 
@@ -118,54 +116,4 @@ contract SoundCoin is StandardToken {
         if(!_spender.call(bytes4(bytes32(keccak256("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { revert("Err!"); }
         return true;
     }
-}
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
-    }
-
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
 }
